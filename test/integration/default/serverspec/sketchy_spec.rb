a151d5644cecfacd4fd4cb7357e607c309c97b93
juju4 <juju4@users.noreply.github.com>
2020-05-22 21:29:39 -0400

    test/default: re-enable serverspec for all

--- test/integration/default/serverspec/sketchy_spec.rb
+++ test/integration/default/serverspec/sketchy_spec.rb
@@ -16,8 +16,13 @@ describe command('curl -sSq http://localhost:8000/eager | tee /tmp/sketchy-root.
   its(:exit_status) { should eq 0 }
 end
 
-# Ubuntu 18.04: blocks on '/usr/local/bin/phantomjs --ssl-protocol=any --ignore-ssl-errors=yes /var/sketchy/git/sketchy/assets/capture.js http://google.com /var/sketchy/git/files/google.com'
-describe command('curl -sSq "http://localhost:8000/eager?url=http://google.com&type=html" | tee /tmp/sketchy-capture.out'), :if => os[:family] == 'ubuntu' && os[:release] != '18.04' do
+describe command('curl -sSq "http://localhost:8000/eager?url=http://google.com&type=html" | tee /tmp/sketchy-capture.out') do
+  its(:stdout) { should match /<title>Google<\/title>/ }
+  its(:stderr) { should_not match /Empty reply from server/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('curl -sSq "http://localhost:8000/eager?url=http://wikipedia.org&type=html" | tee /tmp/sketchy-capture.out') do
   its(:stdout) { should match /<title>Google<\/title>/ }
   its(:stderr) { should_not match /Empty reply from server/ }
   its(:exit_status) { should eq 0 }
