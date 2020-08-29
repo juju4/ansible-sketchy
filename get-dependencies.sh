27b5821e024825c6fd2282e10bc98fccd84577d3
juju4 <juju4@users.noreply.github.com>
2020-08-29 12:00:17 -0400

    centos8: needs EPEL for qt5-qtwebkit-devel

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.sketchy ] && ln -s ansible-sketchy $rolesdir/juju4.sketchy
 [ ! -e $rolesdir/juju4.sketchy ] && cp -R $rolesdir/ansible-sketchy $rolesdir/juju4.sketchy
